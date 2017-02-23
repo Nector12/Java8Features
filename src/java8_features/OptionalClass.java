371d7e00794d64b8a329bc752d06eba98277b340
Nector12 <rarquesvall@gmail.com>
2017-02-23 21:31:27 +0100

    Optional class

---
+++ src/java8_features/OptionalClass.java
@@ -0,0 +1,46 @@
+package java8_features;
+
+import java.util.Optional;
+
+/**
+ * Created by nector on 23/02/17.
+ */
+public class OptionalClass {
+
+    public static void main(String[] args) {
+
+        OptionalClass optionalClass = new OptionalClass();
+        optionalClass.optionalParam(Optional.ofNullable(15));
+        optionalClass.optionalParam(Optional.ofNullable(null));
+
+        Optional<String> string = optionalClass.optionalReturn(false);
+        if (string.isPresent()) {
+            System.out.println("Result not null");
+        } else {
+            System.out.println("Result null");
+        }
+
+    }
+
+    private void optionalParam(Optional<Integer> optParam) {
+
+        if (optParam.isPresent()) {
+            System.out.println("You passed in a parameter");
+        } else {
+            System.out.println("The parameter is null");
+        }
+
+    }
+
+    /**
+     *
+     * @param b
+     * @return a valid string if b == true, else null
+     */
+    private Optional<String> optionalReturn(boolean b) {
+
+        return b ? Optional.of("This is a string") : Optional.empty();
+
+    }
+
+}
